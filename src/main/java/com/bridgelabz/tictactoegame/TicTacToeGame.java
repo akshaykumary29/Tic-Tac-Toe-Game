d446f89213b9e1d2a861c1dd253b2e058c597e33
akshay <akshay.yamgar29@gmail.com>
2021-10-02 00:17:57 +0530

    Added: Creating Tic Tac Toe Board

--- src/main/java/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -3,5 +3,16 @@ package com.bridgelabz.tictactoegame;
 public class TicTacToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe Game!!!");
+        createBoard();
+    }
+
+    // Creating empty tic-tac-toe board
+    public static void createBoard() {
+        char[] board = new char[10];
+
+        // initialize an array
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
     }
 }
